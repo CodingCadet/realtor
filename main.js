60b6aba4165f96f8eb77dff3f86dd8cea71f4635
Coding Cadet Cypress <cypress@codingcadet.com>
2016-11-02 00:59:01 +0000

    listings js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function ppsq(price, sqft) {
-    return price / sqft;
+function removeCommas(num) {
+    var result = num.replace(/,/g, "");
+    //used RegEx above to mean /,/g = ","
+    return result;
 }
 
 $(document).ready(function() {
-    $(".listing-item .row").each(function(e) {
-        console.log(e);
-        console.log($(this));
+    $(".listing-item .row").each(function(i, element) {
+        var sqft = $(element).find(".sqft").text();
+        var price = $(element).find(".price").text();
+        var formattedPrice = removeCommas(price);
+        var ppsq = formattedPrice / sqft;
+        $(element).find(".ppsq").text(ppsq.toFixed(2));
     });
 });
 
